d00f37f857453ae902bee78e11196829c2c66087
Mathis Chambon <matchambon60@gmail.com>
2024-06-17 16:38:25 +0200

    Init storybook stack

---
+++ .storybook/preview.ts
@@ -0,0 +1,54 @@
+import { Preview } from '@storybook/html';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import Twig from 'twig';
+
+import '../components/base.css';
+
+Twig.cache();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    },
+    backgrounds: {
+      default: 'white',
+      values: [
+        {
+          name: 'grey',
+          value: '#F5F5F5'
+        },
+        {
+          name: 'grey-lighter',
+          value: '#D6D6D6'
+        },
+        {
+          name: 'white',
+          value: '#FFFFFF'
+        },
+        {
+          name: 'theme-lightest',
+          value: '#FFF5F1'
+        }
+      ]
+    },
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        largeDesktop: {
+          name: 'Large desktop',
+          styles: {
+            width: '1681px',
+            height: '801px'
+          }
+        }
+      },
+      defaultViewport: 'desktop'
+    }
+  }
+};
+
+export default preview;
