35fc21c0eb96a7795cd72136b7e2193cff5f4834
Mathis Chambon <matchambon60@gmail.com>
2024-06-24 16:07:15 +0200

    feat(fields): wip, need tooltips and buttons

---
+++ components/Molecules/Fields/FieldInput/FieldInput.stories.js
@@ -0,0 +1,74 @@
+import FieldInput from './FieldInput.twig';
+import fieldInputFunction from './FieldInput.js';
+
+export default {
+  title: 'Design System/Molecules/Fields/FieldInput'
+};
+
+export const Base = {
+  render: (args) => FieldInput(args),
+  play: () => {
+    fieldInputFunction();
+  },
+  args: {
+    name: 'indication',
+    type: '',
+    label: 'Indication',
+    placeholder: 'Ex. Nom',
+    iconButtonLeft: false,
+    min: '',
+    max: '',
+    error: '',
+    unit: '',
+    tooltip: '',
+    iconButton: ''
+  },
+  argTypes: {
+    disabled: {
+      control: { type: 'boolean' }
+    },
+    required: {
+      control: { type: 'boolean' }
+    },
+    success: {
+      control: { type: 'boolean' }
+    },
+    size: {
+      options: ['large', 'small'],
+      control: { type: 'select' }
+    }
+  }
+};
+
+export const WithButton = {
+  render: (args) => FieldInput(args),
+  play: () => {
+    fieldInputFunction();
+  },
+  args: {
+    name: 'promoCode',
+    type: 'text',
+    label: '',
+    placeholder: 'Code Promo',
+    min: '',
+    max: '',
+    error: '',
+    tooltip: '',
+    button: 'Appliquer'
+  },
+  argTypes: {
+    disabled: {
+      control: { type: 'boolean' }
+    },
+    required: {
+      control: { type: 'boolean' }
+    },
+    success: {
+      control: { type: 'boolean' }
+    },
+    size: {
+      options: ['large', 'small'],
+      control: { type: 'select' }
+    }
+  }
+};
