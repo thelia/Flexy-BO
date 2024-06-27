7060bcc08ab9fbaf0c4ebf3a525c1f5eebc2f719
Mathis Chambon <matchambon60@gmail.com>
2024-06-27 14:33:34 +0200

    fix(social-media): add size config

--- components/Molecules/SocialMedia/SocialMedia.stories.js
+++ components/Molecules/SocialMedia/SocialMedia.stories.js
@@ -5,16 +5,34 @@ export default {
   title: 'Design System/Molecules/SocialMedia'
 };
 
-export const list = () => SocialMedias();
+export const list = {
+  render: (args) => SocialMedias(args),
+  args: { size: 'w-4 h-4' },
+  argTypes: {
+    size: {
+      control: {
+        type: 'select'
+      },
+      options: ['w-8 h-8', 'w-10 h-10', 'w-14 h-14', 'w-20 h-20']
+    }
+  }
+};
 
 export const base = {
   render: (args) => SocialMedia(args),
   args: {
+    size: 'w-4 h-4',
     variant: 'black',
     icon: 'facebook',
     href: '#'
   },
   argTypes: {
+    size: {
+      control: {
+        type: 'select'
+      },
+      options: ['w-8 h-8', 'w-10 h-10', 'w-14 h-14', 'w-20 h-20']
+    },
     variant: {
       control: {
         type: 'select'
