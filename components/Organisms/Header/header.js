4bfdf641471e5cbff86b7e0a098f7bf7f835bba8
Mathis Chambon <matchambon60@gmail.com>
2024-07-15 11:55:13 +0200

    feat(header): sidebar interactivity

--- components/Organisms/Header/header.js
+++ components/Organisms/Header/header.js
@@ -5,6 +5,17 @@ export const header = () => {
   const menu = header.querySelector('.Header-left-menu');
   const sidebar = document.querySelector('.Header-sidebar');
   const sidebarClose = sidebar.querySelector('.Header-sidebar-close');
+  const sidebarTitle = sidebar.querySelector('.Header-sidebar-title');
+  const sidebarBack = sidebar.querySelector('.Header-sidebar-back');
+  const sidebarMenu = sidebar.querySelector('.SidebarMenu');
+  const configMenu = sidebar.querySelector('.ConfigMenu');
+  const langMenu = sidebar.querySelector('.LangMenu');
+  const configButton = sidebar.querySelector(
+    '.Header-sidebar-menus-config > .Button'
+  );
+  const langButton = sidebar.querySelector(
+    '.Header-sidebar-menus-lang > .Button'
+  );
 
   openSearchbar.addEventListener('click', () => {
     header.classList.add('Header-searchbar--open');
@@ -34,5 +45,43 @@ export const header = () => {
 
   sidebarClose.addEventListener('click', () => {
     sidebar.classList.remove('Header-sidebar--open');
+    sidebarTitle.textContent = 'Menu';
+    sidebarClose.classList.add('Header-sidebar-close--withoutBack');
+    sidebarMenu.classList.add('SidebarMenu--open');
+    configMenu.classList.remove('ConfigMenu--open');
+    langMenu.classList.remove('LangMenu--open');
+    sidebarBack.classList.remove('Header-sidebar-back--active');
+  });
+
+  configButton.addEventListener('click', () => {
+    configMenu.classList.toggle('ConfigMenu--open');
+    sidebarBack.classList.add('Header-sidebar-back--active');
+    sidebarClose.classList.remove('Header-sidebar-close--withoutBack');
+
+    sidebarTitle.textContent = 'Configuration';
+
+    sidebarMenu.classList.remove('SidebarMenu--open');
+    langMenu.classList.remove('LangMenu--open');
+  });
+
+  langButton.addEventListener('click', () => {
+    langMenu.classList.toggle('LangMenu--open');
+    sidebarBack.classList.add('Header-sidebar-back--active');
+    sidebarClose.classList.remove('Header-sidebar-close--withoutBack');
+
+    sidebarTitle.textContent = 'Langue';
+
+    sidebarMenu.classList.remove('SidebarMenu--open');
+    configMenu.classList.remove('ConfigMenu--open');
+  });
+
+  sidebarBack.addEventListener('click', () => {
+    sidebarBack.classList.remove('Header-sidebar-back--active');
+    sidebarTitle.textContent = 'Menu';
+
+    sidebarClose.classList.add('Header-sidebar-close--withoutBack');
+    sidebarMenu.classList.add('SidebarMenu--open');
+    configMenu.classList.remove('ConfigMenu--open');
+    langMenu.classList.remove('LangMenu--open');
   });
 };
