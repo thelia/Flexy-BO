c270363462dc7feaec7b3dc20871051ae6a1bb55
Mathis Chambon <matchambon60@gmail.com>
2024-07-17 11:00:57 +0200

    feat(header): arrow rotate

--- components/Molecules/UserMenuButton/userMenuButton.js
+++ components/Molecules/UserMenuButton/userMenuButton.js
@@ -2,8 +2,16 @@ export const userMenuButton = () => {
   const buttons = document.querySelectorAll('.UserMenuButton');
 
   buttons.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
       button.classList.toggle('UserMenuButton--active');
     });
+  });
+
+  window.addEventListener('click', (e) => {
+    if (!e.target.closest('.UserMenuButton')) {
+      buttons.forEach((button) => {
+        button.classList.remove('UserMenuButton--active');
+      });
+    }
   });
 };
