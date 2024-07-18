7ec7c8deeaa7dd1c34ffd892d84980930ceb5ed2
Mathis Chambon <matchambon60@gmail.com>
2024-07-18 10:44:59 +0200

    feat(layout): header / sidebar / footer

--- components/Layouts/Layout/Layout.stories.js
+++ components/Layouts/Layout/Layout.stories.js
@@ -1,3 +1,15 @@
+import { header } from '../../Organisms/Header/header';
+import { sidebar } from '../../Organisms/Sidebar/sidebar';
+import Layout from './Layout.twig';
+
 export default {
   title: 'Design System/Layouts/Layout'
+};
+
+export const base = {
+  render: (args) => Layout(args),
+  play: () => {
+    header();
+    sidebar();
+  }
 };
